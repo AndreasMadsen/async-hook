1e2a53970849413fef8d33f3e6b925bffecf5635
Andreas Madsen <amwebdk@gmail.com>
2016-06-28 15:46:42 +0200

    [fix] update to node 6

--- test/test-promise-timing.js
+++ test/test-promise-timing.js
@@ -6,8 +6,8 @@ const assert = require('assert');
 const eventOrder = [];
 
 asyncHook.addHooks({
-  init: function (uid) {
-    eventOrder.push(`init#${uid}`);
+  init: function (uid, handle) {
+    eventOrder.push(`init#${uid} ${handle.constructor.name}`);
   },
   pre: function (uid) {
     eventOrder.push(`pre#${uid}`);
@@ -26,25 +26,29 @@ new Promise(function (s) {
   setTimeout(s, 100); // 1
 })
 .then(function () {
-  return new Promise((s) => setTimeout(s, 100)); // 2
+  return new Promise((s) => setTimeout(s, 100)); // 4
 })
 .then();
 
 process.once('exit', function () {
   assert.deepEqual(eventOrder, [
-    'init#-1',
-    'init#-2',
-    'init#-3',
+    'init#-1 TimeoutWrap',
+    'init#-2 PromiseWrap',
+    'init#-3 PromiseWrap',
     'pre#-1',
     'post#-1',
     'destroy#-1',
     'pre#-2',
-    'init#-4',
+    'init#-4 TimeoutWrap',
     'post#-2',
     'destroy#-2',
+    'init#-5 PromiseWrap',
     'pre#-4',
     'post#-4',
     'destroy#-4',
+    'pre#-5',
+    'post#-5',
+    'destroy#-5',
     'destroy#-3'
   ]);
 });
