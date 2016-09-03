81ca36141a6d026abdc4b1317fc0c6a6e276fa41
long.qul <long.qul@alibaba-inc.com>
2016-09-03 15:48:39 +0800

    fix test

--- test/test-promise-timing.js
+++ test/test-promise-timing.js
@@ -31,6 +31,7 @@ new Promise(function (s) {
 .then();
 
 process.once('exit', function () {
+  process._rawDebug(eventOrder);
   assert.deepEqual(eventOrder, [
     'init#-1 TimeoutWrap',
     'init#-2 PromiseWrap',
@@ -42,13 +43,9 @@ process.once('exit', function () {
     'init#-4 TimeoutWrap',
     'post#-2',
     'destroy#-2',
-    'init#-5 PromiseWrap',
     'pre#-4',
     'post#-4',
     'destroy#-4',
-    'pre#-5',
-    'post#-5',
-    'destroy#-5',
     'destroy#-3'
   ]);
 });
