5d7695a1ebefd83f8aaf36c745938c416ef0d525
Andreas Madsen <amwebdk@gmail.com>
2016-09-13 15:10:46 +0200

    [test] move exception asserting from runner to test script

--- test/runner.js
+++ test/runner.js
@@ -38,19 +38,8 @@ function runTest(filename, done) {
     stdio: ['ignore', 1, 2]
   });
 
-  // test cases that are expected to exit with status code 1
-  const exceptionCases = [
-    'test-nexttick-exception.js',
-    'test-timeout-exception.js'
-  ];
-
   p.once('close', function (statusCode) {
-    const ok = function () {
-      if(exceptionCases.indexOf(filename) > -1) {
-        return statusCode === 1;
-      }
-      return statusCode === 0;
-    }();
+    const ok = (statusCode === 0);
 
     if (ok) {
       console.log(' ' + passed('ok'));
