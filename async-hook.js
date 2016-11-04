eb317e71646f424f6a52f36715196145825da6d1
Andreas Madsen <amwebdk@gmail.com>
2016-11-04 21:42:54 +0100

    [fix] add warning about unstable API

--- async-hook.js
+++ async-hook.js
@@ -103,6 +103,7 @@ function AsyncHook() {
   }
 
   // setup async wrap
+  console.error('warning: you are using async-hook which is unstable.');
   asyncWrap.setupHooks({
     init: this._hooks.init,
     pre: this._hooks.pre,
